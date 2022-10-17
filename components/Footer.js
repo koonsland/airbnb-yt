40eb6674d556ec7ca536626a7303e0dd5de19d14
박근성 <geunsungpark@theswing.co.kr>
2022-10-17 22:23:18 +0900

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div
+      className="grid grid-cols-1 md:grid-cols-4 space-y-10 px-32 py-14 bg-gray-100
+     text-gray-600"
+    >
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5>ABOUT</h5>
+        <p>How Airbnbn works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className=" space-y-4 text-xs text-gray-800">
+        <h5>COMMUNITY</h5>
+        <p>How Airbnbn works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className=" space-y-4 text-xs text-gray-800">
+        <h5>HOST</h5>
+        <p>How Airbnbn works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className=" space-y-4 text-xs text-gray-800">
+        <h5>HELP CENTER</h5>
+        <p>How Airbnbn works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
